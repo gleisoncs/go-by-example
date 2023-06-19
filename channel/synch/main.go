7a63a2f0ae610703a038c9da5671247eae405cbe
gleisoncs <gleisoncs@gmail.com>
2023-06-19 09:53:46 -0300

    channel sync

---
+++ channel/synch/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Print("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	done <- true
+}
+
+func main() {
+
+	done := make(chan bool)
+	go worker(done)
+	fmt.Println("not yet")
+
+	<-done
+}
